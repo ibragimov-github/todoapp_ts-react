7fca4dcc362bbe4b19672f1f84ada892de51384e
Murad Ibragimov <texet0598@gmail.com>
2022-08-31 10:13:13 +0300

    small change in interface

--- src/components/InputLabel/InputLabel.tsx
+++ src/components/InputLabel/InputLabel.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './InputLabel.module.scss';
-import {v4} from 'uuid';
+import { v4 } from 'uuid';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import TodoItem from '../TodoItem/TodoItem';
@@ -18,24 +18,24 @@ function InputLabel() {
   const [list, setList] = useState('');
   const clearComplete = () => {
     setTodos([...todos.filter((todo: any) => {
-      if(!todo.check) return todo;
+      if (!todo.check) return todo;
     })])
   }
   const handleChange = (event: any, newAlignment: any) => {
     setAlignment(newAlignment);
   };
   const showList = () => {
-    if(list) {setList('')}
-    else {setList('active')}
+    if (list) { setList('') }
+    else { setList('active') }
   }
   const todoSend = (event: React.KeyboardEvent<HTMLElement>) => {
-    if(event.key === 'Enter' && value.trim()) {
+    if (event.key === 'Enter' && value.trim()) {
       const newTodo = {
         id: v4(),
         content: value,
         check: false
       }
-      setTodos([...todos, newTodo])
+      setTodos([newTodo, ...todos])
       setValue('');
     }
   };
@@ -43,55 +43,16 @@ function InputLabel() {
     <div className={styles.container}>
       <div className={styles.input}>
         <button
-          onClick={()=>showList()}
+          onClick={() => showList()}
         ></button>
-        <input 
-          type="text" 
+        <input
+          type="text"
           placeholder='What needs to be done'
           value={value}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
           onKeyDown={todoSend}
         />
       </div>
-      <ul 
-        className={`${styles.list} ${styles[`${list}`]}`}
-      >
-        {todos.map((el: Todo) => {
-          if (alignment === 'all') {
-          return (
-            <TodoItem
-              key={el.id}
-              el={el}
-              todos={todos}
-              setTodos={setTodos}
-            />
-          )}
-          else if(alignment === 'active') {
-            if(!el.check) {
-              return (
-                <TodoItem
-                  key={el.id}
-                  el={el}
-                  todos={todos}
-                  setTodos={setTodos}
-                />
-              )
-            }
-          }
-        else if (alignment==='completed') {
-          if (el.check) {
-            return (
-              <TodoItem
-                key={el.id}
-                el={el}
-                todos={todos}
-                setTodos={setTodos}
-              />
-            )
-          }
-        }
-        })}
-      </ul>
       <div className={styles.nav}>
         <ToggleButtonGroup
           size='small'
@@ -110,12 +71,51 @@ function InputLabel() {
             value='completed'
           >Completed</ToggleButton>
         </ToggleButtonGroup>
-      <Button 
-        variant="contained"
-        onClick={clearComplete}
-      >Clear completed</Button>
+        <Button
+          variant="contained"
+          onClick={clearComplete}
+        >Clear completed</Button>
       </div>
-
+      <ul
+        className={`${styles.list} ${styles[`${list}`]}`}
+      >
+        {todos.map((el: Todo) => {
+          if (alignment === 'all') {
+            return (
+              <TodoItem
+                key={el.id}
+                el={el}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            )
+          }
+          else if (alignment === 'active') {
+            if (!el.check) {
+              return (
+                <TodoItem
+                  key={el.id}
+                  el={el}
+                  todos={todos}
+                  setTodos={setTodos}
+                />
+              )
+            }
+          }
+          else if (alignment === 'completed') {
+            if (el.check) {
+              return (
+                <TodoItem
+                  key={el.id}
+                  el={el}
+                  todos={todos}
+                  setTodos={setTodos}
+                />
+              )
+            }
+          }
+        })}
+      </ul>
     </div>
   )
 }
